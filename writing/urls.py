7b60846a5685de4ae7afe3af829adaf67c6f8747
Billy Rathje <bwjr@nwlink.com>
2013-05-24 18:58:42 -0700

    Changed styles, URLs

--- writing/urls.py
+++ writing/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 from writing import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
     url(r'^(?P<t>[\w|\W]+)/$', views.paper, name = 'paper'),
 )
--- writing/views.py
+++ writing/views.py
@@ -1,10 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.views import generic
+
 from writing.models import Paper, Comments
 
-def index(request):
-    get_papers = Paper.objects.all().order_by('-time')
-    return render(request, 'writing/index.html', {'get_papers': get_papers})
+class IndexView(generic.ListView):
+    template_name = 'writing/index.html'
+    context_object_name = 'get_papers'
+
+    def get_queryset(self):
+        return Paper.objects.all().order_by('-time')
 
 def paper(request, t):
     get_paper = get_object_or_404(Paper, title=t)
